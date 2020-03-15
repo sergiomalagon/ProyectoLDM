6cf7b442d3a7e6769c3d9a73bf8af52b232f3afb
Sergio Malagon <sergiomalagonmartin@gmail.com>
2020-03-15 10:47:16 +0100

    15/03/2020

--- menu.js
+++ menu.js
@@ -1,19 +1,3 @@
-
-
-/*
-function mostrarMenu() {
-  if (document.getElementById("indice").hidden == false) {
-    document.getElementById("indice").hidden = true;
-    document.getElementById("ocultarMenu").style["margin-top"] = "6%";
-  } else {
-    document.getElementById("indice").hidden = false;
-    document.getElementById("cultarMenu").style["margin-top"] = "1%";
-
-
-  }
-}
-*/
-
 function mostrarMenu() {
   if (document.getElementById("indiceScrip").hidden == true) {
     document.getElementById("indiceScrip").hidden = false;
@@ -21,20 +5,3 @@ function mostrarMenu() {
     document.getElementById("indiceScrip").hidden = true;
   }
 }
-
-function mostrarTest() {
-   document.getElementById("testFormatos").style["background"]="lightgreen";
-   setTimeout 
- 
-}
-
-
-//function validacion() {
-//var formularios = document.getElementsByName('name');
-//var elementos = document.getElementsByTagName('tagName');
-
-//Array.from(elemetos).forEach(function(valor,indice,array){
-//  console.log("En el indice" + indice + "hay este valor" + valor);
-//}
-
-//}
