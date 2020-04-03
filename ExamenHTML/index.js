8dfdda641cb872104f3dd29bf228d6c66b160539
Sergio Malagon <sergiomalagonmartin@gmail.com>
2020-04-03 17:53:10 +0200

    03/04/2020

---
+++ ExamenHTML/index.js
@@ -0,0 +1,7 @@
+function ocultarLeroy() {
+    if (document.getElementById("leroy").hidden == true) {
+      document.getElementById("leroy").hidden = false;
+    } else {
+      document.getElementById("leroy").hidden = true;
+    }
+  }
